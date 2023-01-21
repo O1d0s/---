0c0a7774b1a5e4b053922e0ca019c58cfdbe883c
Aidos <ajdos0735@gmaul.com>
2023-01-21 23:12:11 +0600

    написал код для ввода длины массиво и ввода строковых данных

---
+++ Program/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.Write("Сколько слов хотите ввести: ");
+int a = int.Parse(Console.ReadLine());
+string[] arr = InputArray(a);
+string[] mas = new string[arr.Length];
+
+Console.WriteLine("Данные первого массива: ");
+PrintArray(arr);
+Console.WriteLine("Данные перезаписанного массива: ");
+PrintArray(mas);
+
+string[] InputArray(int a){
+    string[] arr = new string[a];
+    Console.WriteLine("Введите слова: ");
+    for(int i=0;i<a;i++){
+        arr[i] = Console.ReadLine();
+    }
+    return arr;
+}
+
+
+void PrintArray(string[] array){
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
